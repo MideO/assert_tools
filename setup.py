77e430e04a4461b96ca6f4a738ec09c38fb0587f
mide <mide@alexandalexa.com>
2014-03-05 11:29:46 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='assert_tools',
+    url='http://mideo.github.io/assert_tools/',
     description='A Python Unit/Functional test library.',
     version='0.0.1',
     author='Mide Ojikutu',
@@ -10,12 +11,16 @@ setup(
     install_requires=['unittest2', 'requests', 'httplib2'],
     tests_require=["nose", 'mock', 'coverage', 'nosexcover'],
     classifiers=[
-        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'Programming Language :: Python :: 2 :: Only',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Intended Audience :: Developers',
-        'Intended Audience :: Software Testers',
-        'Topic :: Selenium Server',
-    ]
+        'Topic :: Software Development :: Testing'
+        'Natural Language :: English',
+    ],
+    keywords=[
+        'testing', 'python', 'rest', 'unit testing', 'functional testing']
 )
