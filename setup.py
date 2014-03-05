9c5959cc59111844ef435daf4c2387a144fd47ce
mide <mide@alexandalexa.com>
2014-03-05 11:37:18 +0000

    editing setup.py

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ setup(
     install_requires=['unittest2', 'requests', 'httplib2'],
     tests_require=["nose", 'mock', 'coverage', 'nosexcover'],
     classifiers=[
+        'Topic :: Software Development :: Testing',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2 :: Only',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Unix',
         'Operating System :: POSIX',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Natural Language :: English',
     ],
     keywords=[
         'testing', 'python', 'rest', 'unit testing', 'functional testing']
